3508d57d40fec82723b34a8522d3d40e605d5474
Bryan Mora <brm.988k@gmail.com>
2019-10-19 10:03:07 -0700

    Enter

--- app/login/login.js
+++ app/login/login.js
@@ -1,3 +1,14 @@
+var user;
+var pass;
+const username = "user1";
+const password = "password"
+
 function init() {
-    
+user = document.getElementById("inp-username");
+pass = document.getElementById("inp-password");
+}
+function login(){
+    if(user === username && pass === password){
+        localStorage.setItem("username","user1");
+    }
 }
