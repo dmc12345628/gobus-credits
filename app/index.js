805fdb75f3acde6838874abfca3e6c5413af914e
Jose Maria <jose_102102@hotmail.com>
2019-10-19 12:29:52 -0700

    WIP agregar creditos

--- app/index.js
+++ app/index.js
@@ -2,6 +2,11 @@ function init() {
     if (isUserLoggedIn()) {
         console.log('si');
     } else {
-       ;
+        window.location.href = './login/login.html';
     }
+}
+
+function loadCredit() {
+    var divCreditosValue = document.getElementById("div-creditos-value");
+    divCreditosValue.innerHTML = getCredits();
 }
--- app/storage.js
+++ app/storage.js
@@ -1,4 +1,20 @@
+
 function isUserLoggedIn() {
     const user = localStorage.getItem('user');
     return user !== null ? user : false;
+    
+}
+
+function getCredits() {
+    const credits = localStorage.getItem('credits');
+    return credits !== null ? parseInt(credits) : 0;
+    //esto en un operador ternaro
+}
+
+function setCredits(creditsToAdd) {
+    const actualCredit= getCredits();
+    const credit=creditsToAdd+actualCredit;
+    localStorage.setItem("credits",credit);
+    
+
 }
